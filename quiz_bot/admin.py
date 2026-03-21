e44d5f5ece8fcf8fb9281c111c00abfa7c415c9b
Sardorbek <nsardorbek776@gmail.com>
2026-03-21 23:57:38 +0500

    ok

--- quiz_bot/admin.py
+++ quiz_bot/admin.py
@@ -1,11 +1,60 @@
 from django.contrib import admin
-from quiz_bot.models import *
-# Register your models here.
+from .models import CustomUser, Quizes, QuizQuestion, QuizAnswers, ReadyCount
 
 
-admin.site.register(CustomUser)
-admin.site.register(Quizes)
-admin.site.register(QuizQuestion)
-admin.site.register(QuizAnswers)
-admin.site.register(ReadyCount)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("id", "tg_id", "username", "role")
+    search_fields = ("tg_id", "username")
+    list_filter = ("role",)
 
+
+class QuizQuestionInline(admin.TabularInline):
+    model = QuizQuestion
+    extra = 1
+
+
+@admin.register(Quizes)
+class QuizesAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "deadline", "share_code")
+    search_fields = ("title", "share_code")
+    inlines = [QuizQuestionInline]
+
+
+@admin.register(QuizQuestion)
+class QuizQuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "quiz", "short_question", "correct_index")
+    search_fields = ("question",)
+    list_filter = ("quiz",)
+
+    def short_question(self, obj):
+        return obj.question[:50]
+
+
+@admin.register(QuizAnswers)
+class QuizAnswersAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "quiz",
+        "correct_answers",
+        "wrong_answers",
+        "not_answered",
+        "total_time",
+    )
+    list_filter = ("quiz", "user")
+    search_fields = ("user__username", "quiz__title")
+
+
+@admin.register(ReadyCount)
+class ReadyCountAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "chat_id",
+        "quiz",
+        "count",
+        "is_ended",
+        "quiz_starter",
+    )
+    list_filter = ("is_ended", "quiz")
+    search_fields = ("chat_id",)
