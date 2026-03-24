2f88e5d1779431a204337d49c105b0fc2b5dd3fd
Sardorbek <nsardorbek776@gmail.com>
2026-03-24 07:02:00 +0500

    ok

--- manage.py
+++ manage.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
+import traceback
+import asyncio
+
+
+def global_exception_handler(exctype, value, tb):
+    print("\n🔥 GLOBAL SYNC ERROR 🔥")
+    traceback.print_exception(exctype, value, tb)
+
+
+def async_exception_handler(loop, context):
+    print("\n🔥 GLOBAL ASYNC ERROR 🔥")
+    msg = context.get("exception", context["message"])
+    print(msg)
+    traceback.print_exc()
 
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     os.environ.setdefault('DJANGO_ALLOW_ASYNC_UNSAFE', 'true')
+
+    # 🔥 GLOBAL SYNC ERROR
+    sys.excepthook = global_exception_handler
+
+    # 🔥 GLOBAL ASYNC ERROR
+    loop = asyncio.get_event_loop()
+    loop.set_exception_handler(async_exception_handler)
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
